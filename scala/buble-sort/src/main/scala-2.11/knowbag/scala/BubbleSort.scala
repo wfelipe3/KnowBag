c06d748b1a6717d137f6130469ce28efa021f21d
Felipe Rojas <wfelipe3@gmail.com>
2015-10-02 00:55:33 -0500

    update

--- scala/buble-sort/src/main/scala-2.11/knowbag/scala/BubbleSort.scala
+++ scala/buble-sort/src/main/scala-2.11/knowbag/scala/BubbleSort.scala
@@ -12,13 +12,18 @@ object BubbleSort {
       }
     else list
 
-  private def sort(list: Seq[Int], pivot: Int, index: Int, n: Int): Seq[Int] =
-    if (index == n)
-      list
-    else if (list(pivot) > list(index))
-      sort(swap(list, index, pivot), pivot = index, index = index + 1, n)
-    else
-      sort(list, pivot = index, index = index + 1, n)
+  private def sort(list: Seq[Int], pivot: Int, index: Int, n: Int): Seq[Int] = {
+    def sort(list: Seq[Int], pivot: Int, index: Int): Seq[Int] =
+      if (index == n)
+        list
+      else if (list(pivot) > list(index))
+        sort(swap(list, index, pivot), pivot = index, index = index + 1)
+      else
+        sort(list, pivot = index, index = index + 1)
+
+    sort(list, pivot, index)
+  }
+
 
   private def swap(list: Seq[Int], rightIndex: Int, leftIndex: Int) =
     list.updated(rightIndex, list(leftIndex)).updated(leftIndex, list(rightIndex))
