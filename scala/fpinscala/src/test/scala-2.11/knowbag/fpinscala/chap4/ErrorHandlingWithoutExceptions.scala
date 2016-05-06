80b2d0d8a684b5ca2a2d9f718953fa5005f54dba
williame <william.rojas@bizagi.com>
2016-05-06 08:35:20 -0500

    chapter 4 Error handling without exceptions

---
+++ scala/fpinscala/src/test/scala-2.11/knowbag/fpinscala/chap4/ErrorHandlingWithoutExceptions.scala
@@ -0,0 +1,10 @@
+package knowbag.fpinscala.chap4
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+  * Created by WilliamE on 06/05/2016.
+  */
+class ErrorHandlingWithoutExceptions extends FlatSpec with Matchers {
+
+}
