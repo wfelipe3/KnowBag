c4e2950acd3eb38008eddb08d23f10543c47df9e
williame <william.rojas@bizagi.com>
2016-06-01 09:09:31 -0500

    Either left and right implementation

--- scala/fpinscala/src/test/scala-2.11/knowbag/fpinscala/chap4/ErrorHandlingWithoutExceptions.scala
+++ scala/fpinscala/src/test/scala-2.11/knowbag/fpinscala/chap4/ErrorHandlingWithoutExceptions.scala
@@ -1,6 +1,6 @@
 package knowbag.fpinscala.chap4
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{FlatSpec, Matchers}
 
 /**
   * Created by WilliamE on 06/05/2016.
@@ -20,6 +20,21 @@ class ErrorHandlingWithoutExceptions extends FlatSpec with Matchers {
     val sum: (Int, Int) => Int = (a, b) => a + b
     val sumOption: Option[Int] = map2(Some(1), Some(2))(sum)
     sumOption should be(Some(3))
+  }
+
+  "Exercise 4.4" should "implement sequence that combines a list of options into an option containing a list" in {
+    sequence(List(Some(1), Some(2), Some(3))) should be(Some(List(1, 2, 3)))
+    sequence(List(Some(1), None, Some(3))) should be(None)
+  }
+
+  "Exercise 4.5" should "implement traverse" in {
+    traverse(List("1", "2", "3", "4"))(a => Try(a.toInt)) should be(Some(List(1, 2, 3, 4)))
+    traverse(List("1", "test"))(a => Try(a.toInt)) should be(None)
+  }
+
+  "Exercise 4.6" should "implement functions in Either" in {
+    Either.Try(10).map(_ + 10) should be(Right(20))
+    Right("test").map2(Right(" this is"))(_ + _) should be(Right("test this is"))
   }
 
   def mean(xs: Seq[Double]): Option[Double] =
@@ -78,5 +93,49 @@ class ErrorHandlingWithoutExceptions extends FlatSpec with Matchers {
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a.flatMap(va => b.map(vb => f(va, vb))).orElse(None)
 
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(v => v)
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight(Try(List[B]()))((av, b) => f(av).flatMap(e => b.map(i => e :: i)))
 }
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Right(v) => Right(f(v))
+      case Left(e) => Left(e)
+    }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(v) => f(v)
+      case Left(e) => Left(e)
+    }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(v) => Right(v)
+      case Left(e) => b
+    }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    this match {
+      case Right(v) => b.map(bv => f(v, bv))
+      case Left(e) => Left(e)
+    }
+}
+
+object Either {
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch {
+      case e: Exception => Left(e)
+    }
+}
+
+case class Left[+E](values: E) extends Either[E, Nothing]
+
+case class Right[+A](value: A) extends Either[Nothing, A]
+
 
