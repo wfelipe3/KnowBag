609005dfc15d8d2c1346942d9a9a9b4b88ca7a9c
Felipe Rojas <feliperojas@iMac-de-Felipe.local>
2016-09-24 17:20:35 -0500

    python pf -> take function with multiple state chages

--- python/fp_python/game/core.py
+++ python/fp_python/game/core.py
@@ -1,35 +1,54 @@
-from pyrsistent import PClass, pmap_field, field, pmap
+from pyrsistent import PClass, pmap_field, field, pmap, pvector_field
+
+
+class Thing(PClass):
+    name = field(str)
 
 
 class Location(PClass):
     name = field(str)
     description = field(str)
     exits = pmap_field(str, str)
+    items = pmap_field(str, Thing)
 
 
+key = Thing(name="rusty key")
 home = Location(name="Home", description="Home is where the heart is!", exits={"east": "Street"})
-street = Location(name="Street", description="The street next to your house.", exits={"west": "Home"})
+street = Location(name="Street", description="The street next to your house.", exits={"west": "Home"},
+                  items={key.name: key})
 
 
 class GameState(PClass):
     location = field(Location)
     world = pmap_field(str, Location)
+    inventory = pvector_field(Thing)
 
 
 world = pmap({x.name: x for x in [home, street]})
-initial_state = GameState(location=home, world=world)
+
+ROOM_FORMAT = """
+* {name} *
+{description}
+
+Exits:
+{exits}
+
+Items here: {items}
+Your inventory: {inventory}
+"""
 
 
 def render(state):
     exits = "\n".join('* {} to {}'.format(exit, location) for exit, location in state.location.exits.items())
-    return "* {name} *\n\n{description}\n\nExits:\n{exits}".format(
+    items = ', '.join(state.location.items.keys())
+    inventory = ", ".join(item.name for item in state.inventory)
+    return ROOM_FORMAT.format(
         name=state.location.name,
         description=state.location.description,
-        exits=exits
+        exits=exits,
+        items=items,
+        inventory=inventory
     )
-
-
-print(render(initial_state))
 
 
 def move(state, exit_name):
@@ -37,7 +56,27 @@ def move(state, exit_name):
     if location_name is None:
         return None
     target_location = state.world.get(location_name)
-    return GameState(location=target_location, world=state.world)
+    return state.set(location=target_location)
 
 
-print(render(move(initial_state, "east")))
+def take(state, item_name):
+    item = state.location.items.get(item_name)
+    if item is None:
+        return None
+    new_items = state.location.items.remove(item_name)
+    new_location = state.location.set(items=new_items)
+    new_inventory = state.inventory.append(item)
+    new_world = state.world.set(new_location.name, new_location)
+    return state.set(
+        location=new_location,
+        inventory=new_inventory,
+        world=new_world
+    )
+
+
+initial_state = GameState(location=home, world=world)
+in_street = move(initial_state, "east")
+after_taken = take(in_street, "rusty key")
+print(render(in_street))
+print(render(after_taken))
+print(render(move(move(after_taken, "west"), "east")))
