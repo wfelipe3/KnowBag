0cf10e8a57076b68dc3ce9327869496027285c96
Felipe Rojas <wfelipe3@gmail.com>
2018-02-11 22:32:49 -0500

    get mail parallel

--- scala/mail-reader/build.sbt
+++ scala/mail-reader/build.sbt
@@ -5,3 +5,7 @@ version := "1.0"
 scalaVersion := "2.12.4"
 
 libraryDependencies += "javax.mail" % "mail" % "1.4"
+
+libraryDependencies += "com.google.apis" % "google-api-services-gmail" % "v1-rev81-1.23.0"
+libraryDependencies += "com.google.oauth-client" % "google-oauth-client-jetty" % "1.23.0"
+libraryDependencies += "com.google.api-client" % "google-api-client" % "1.23.0"
--- scala/mail-reader/src/main/scala/knowbag/mail/MailMain.scala
+++ scala/mail-reader/src/main/scala/knowbag/mail/MailMain.scala
@@ -1,6 +1,23 @@
 package knowbag.mail
 
+import java.io.{File, FileInputStream, InputStreamReader}
+import java.util
+import java.util.Date
+import java.util.concurrent.Executors
 import javax.mail.{Folder, Message, Session}
+
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
+import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeFlow, GoogleClientSecrets}
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.store.FileDataStoreFactory
+import com.google.api.services.gmail.{Gmail, GmailScopes}
+
+import scala.collection.JavaConverters._
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * Created by feliperojas on 2/8/18.
@@ -17,27 +34,89 @@ object MailMain extends App {
   val inbox = store.getFolder("Inbox")
   inbox.open(Folder.READ_ONLY)
 
-  inbox.search { msg =>
-    println(msg.getFrom.map(_.toString).mkString(","))
-    Option(msg.getSubject)
-      .filter(s => s.toUpperCase().contains("BBVA"))
-      .exists(_ => true)
+
+  def getMails(mails: Array[Message], inbox: Folder): Array[Message] = {
+    inbox.search({ msg =>
+      Option(msg.getSubject)
+        .filter(s => s.toUpperCase().contains("BBVA"))
+        .exists(_ => true)
+    }, mails)
   }
-    .toStream
-    .take(100)
-    .map(message)
+
+  val count = inbox.getMessageCount
+
+  private val pool = Executors.newFixedThreadPool(10)
+  val executor = ExecutionContext.fromExecutor(pool)
+
+  val fs = (0 to 99).map { i =>
+    Future {
+      val inbox = store.getFolder("Inbox")
+      inbox.open(Folder.READ_ONLY)
+      println(s"start execution for $i")
+      val start = System.currentTimeMillis()
+      val mails = getMails(inbox.getMessages(count - ((i + 1) * 100), count - (i * 100)), inbox)
+        .map(message)
+      println(s"finish execution for $i time ${System.currentTimeMillis() - start}")
+      inbox.close(true)
+      mails
+    }(executor)
+  }
+
+  Await.result(Future.sequence(fs), Duration.Inf)
+    .flatten
+    .sortBy(_.date)
     .foreach(println)
 
-  //  inbox.getMessages()
-  //    .toStream
-  //    .take(100)
-  //    .map(message)
-  //    .foreach(println)
 
   inbox.close(true)
 
-  def message(m: Message) = {
-    s"subject: {${m.getSubject}}"
+  pool.shutdown()
+
+  case class TestMessage(subject: String, content: String, date: Date)
+
+  def message(m: Message) =
+    TestMessage(m.getSubject, m.getContent.toString, m.getReceivedDate)
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  private def gmailTest = {
+    val SCOPES = util.Arrays.asList(GmailScopes.GMAIL_LABELS)
+    val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport()
+    val DATA_STORE_DIR = new File(System.getProperty("user.home"), ".credentials/gmail-java-quickstart")
+    val DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR)
+    val JSON_FACTORY = JacksonFactory.getDefaultInstance
+
+    def authorize() = {
+      val clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(new FileInputStream("/Users/feliperojas/git/KnowBag/scala/mail-reader/client_id.json")))
+      val flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+        .setDataStoreFactory(DATA_STORE_FACTORY)
+        .setAccessType("offline")
+        .build()
+
+      val credentials = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user")
+      println(s"Credentials saves to ${DATA_STORE_DIR.getAbsolutePath}")
+
+      credentials
+    }
+
+    def getGmailService() = {
+      val credential = authorize()
+      new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+        .setApplicationName("gmail-reader")
+        .build()
+    }
+
+    val gmail = getGmailService()
+    val listResponse = gmail.users().labels().list("me").execute()
+    val labels = listResponse.getLabels
+
+    if (labels.size() == 0)
+      println("No labels found")
+    else {
+      println("labels")
+      labels.asScala.foreach(l => println(l.getName))
+    }
   }
+
 
 }
